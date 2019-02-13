7475169aa3160376efe77a54d1d4d3a2e613906b
Agnes Helena <agneshelena11@gmail.com>
2019-02-13 08:21:10 -0200

    exercicio js

---
+++ script.js
@@ -0,0 +1,10 @@
+let Degraus = Number(prompt('Digite o tamanho da escada'));
+let modeloMaterial = prompt('Digite o modelo do material');
+let escadinha = '';
+
+
+for(let a = 0; a < Degraus; a++ ){
+    
+    escadinha = escadinha + modeloMaterial ;
+    console.log(escadinha);
+}
